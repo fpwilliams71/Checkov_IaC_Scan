105ea3a7d58f28e90f8ade0d54675a3d4a44bb17
fpwilliams71 <fpwilliams71@yahoo.com>
2022-08-19 17:35:16 +0100

    added files

---
+++ terraform_files/gcp/provider.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+# Specify the provider (GCP, AWS, Azure)
+provider "google" {
+credentials = "${file("fpw-credentials.json")}"
+project = var.gcp_project
+region = var.gcp_region
+}
---
+++ terraform_files/gcp/terraform.tfvars
@@ -0,0 +1,8 @@
+# GCP Settings
+gcp_project = "iac-project-359211"
+gcp_region  = "us-central1"
+# gcp_auth_file = "../auth/fpw-credentials.json"
+
+# Bucket
+bucket-name   = "fpw-global-gsb" 
+storage-class = "REGIONAL"
