f592d2e7a6c6d6da51e2829c1224575440600eeb
fpwilliams71 <fpwilliams71@yahoo.com>
2022-10-13 11:45:05 +0100

    Update aws-database-backup.tf

--- aws-database/aws-database-backup.tf
+++ aws-database/aws-database-backup.tf
@@ -26,7 +26,7 @@ instance_class = "db.t2.micro"
 name = "your-db-name"
 username = "your-db-username"
 password = "your-password"
-publicly_accessible    = true
+publicly_accessible    = false
 skip_final_snapshot    = true
 storage_encrypted    = true
 
